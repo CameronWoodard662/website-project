1bff57d2986017b2790ba2378dfd4d5bbdcaa389
CameronWoodard662 <camwoodard2007@gmail.com>
2025-03-07 16:57:34 -0500

    added menu buttons and fixed images

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const menuButton = document.querySelector("#menu-button");
+const menuLinks = document.querySelector("#menu-container");
 
+menuButton.addEventListener("click", () => {
+  console.log(menuLinks);
+  menuLinks.classList.toggle("hidden");
+  menuLinks.classList.toggle("visible");
+});
